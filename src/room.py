595560e4c9280d95520f92a8b8ad5aa18e63dbf8
Justin Smith <jtsmith1287@gmail.com>
2013-12-23 16:34:05 -0800

    Working on rooms

---
+++ src/room.py
@@ -0,0 +1,29 @@
+'''
+Created on Dec 23, 2013
+
+@author: jusmith
+'''
+
+class Room:
+  
+  def __init__(self, name, **kwargs):
+    
+    self.coords = (0,0,0)
+    self.name = name
+    self.contents = set()
+    self.exits = {}
+  
+  def addObj(self, obj):
+    
+    self.contents.add(obj)
+  
+  def removeObj(self, obj):
+    
+    self.contents.discard(obj)
+  
+  def getDisplay(self):
+    
+    output = []
+    output.append(self.name)
+    
+    return "\n".join(output)
---
+++ src/world.py
@@ -0,0 +1,32 @@
+'''
+Created on Dec 23, 2013
+
+@author: jusmith
+'''
+
+import room
+
+
+class World:
+  
+  rooms = {}
+
+  def newRoom(self, name, **kwargs):
+    
+    new_room = room.Room(name, **kwargs)
+    self.rooms[str(new_room.coords)] = new_room
+
+  def moveObj(self, loc, obj):
+    
+    room = self.rooms.get(str(loc))
+    old_room = self.rooms.get(str(obj.location))
+    if old_room:
+      old_room.removeObj(obj)
+    if room:
+      room.addObj(obj)
+
+  def build(self):
+    
+    # this will be where the data will be read and loaded
+    # when there's a pre-exisitng world
+    self.newRoom("Starting Room")
